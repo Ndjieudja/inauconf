126e6d41295b3bf2d4d7c627f2295030b542d2d9
Ndjieudja <gabrielndjieudja@gmail.com>
2023-04-12 00:52:51 +0200

    add readme and readme and fonction to read file

--- src/inauconf/shuffle.py
+++ src/inauconf/shuffle.py
@@ -1,102 +1,131 @@
-# --*-- encodng: urf8 --*--
+# --*-- encodng: utf8 --*--
 # !/usr/bin/env python3
 
-# =================== Cript Username ====================
-
 from random import randrange
+import os, stat
 
-class EncriptAll():
+class EncryptAll():
+	
 	def __init__(self):
 
-		# define differents var to generate criptfile and keys
+		""" 
+			Define differents var to generate CryptData 
+		"""
+		self.code_data = None
+		self.uncode_data = None
 
-		# var from input
-		self.account_ = None
-		self.cript_password = None
-		self.uncript_password = None
-			
 
-		# beginning handing process
-
-	# Afther calling this function we return list values we containt keys and crypt values
-
-	def cript(self, target):
-		# create dic var we contaitn first value and keys to uncrypt values
-
-		# __init__ variours
-			
-		key_val = list()
-		key = list()
-		values = list()
+	def crypt(self, input:str):
 		
-		container_cript = list()
-		for el in range(len(target)):
-			if ord(target[el]) < 10:
-				key_val.append((ord(target[el]), chr(randrange(1, 10))))
-
-			elif ord(target[el]) < 100:
-				interline = randrange(10, 100)
-				if interline in range(48, 58):
-					interline = 71
-				key_val.append((ord(target[el]), chr(interline)))
-
-			elif ord(target[el]) < 1000:
-				key_val.append((ord(target[el]), chr(randrange(100, 1000))))
+		"""
+			Function to code text input
+		"""
+		if type(input) == str:	
+			key_val = list()
+			key = list()
+			values = list()
 			
-			elif ord(target[el]) < 10000:
-				key_val.append((ord(target(el)), chr(randrange(1000, 10000))))
+			container = list()
+			for value in range(len(input)):
+				if ord(input[value]) < 10:
+					key_val.append((ord(input[value]), chr(randrange(1, 10))))
 
-			else: key_val.append((ord(target[el]), chr(randrange(10000, 1000000))))
+				elif ord(input[value]) < 100:
+					interline = randrange(10, 100)
+					if interline in range(48, 58):
+						interline = 71
+					key_val.append((ord(input[value]), chr(interline)))
 
-		for el in key_val:
-			values.append(el[1])
-			values.append(el[0])
+				elif ord(input[value]) < 1000:
+					key_val.append((ord(input[value]), chr(randrange(100, 1000))))
+				
+				elif ord(input[value]) < 10000:
+					key_val.append((ord(input(value)), chr(randrange(1000, 10000))))
 
-		# print(values)
-		string_int_ = [str(int) for int in values]
-		#save_
-		self.cript_password = "".join(string_int_)
+				else: key_val.append((ord(input[value]), chr(randrange(10000, 1000000))))
 
-		return self.cript_password
+			for value in key_val:
+				values.append(value[1])
+				values.append(value[0])
 
+			string_int = [str(int) for int in values]
+			self.code_data = "".join(string_int)
+
+			return self.code_data
+
+		else:
+			print("La valeur a entre dois etre de type String")
+			
 	
-	def uncript(self, target):
-		key = list()
-		keys = list()
-		values = list()
+	def uncrypt(self, input:str):
+	
+		"""
+			Function de uncode data who will code with the same script
+		"""
+		if type(input) == str:
+			key = list()
+			keys = list()
+			values = list()
 
 
-		for el in range(len(target)):
-			try:
-				chr(int((target[el])))
+			for value in range(len(input)):
+				try:
+					chr(int((input[value])))
 
-			except:
-				key.append(ord(target[el]))	
+				except:
+					key.append(ord(input[value]))	
 
-		for el in key:
-			if el < 10 :keys.append(1)
+			for value in key:
+				if value < 10 :keys.append(1)
 
-			elif el < 100:keys.append(2)
+				elif value < 100:keys.append(2)
 
-			elif el < 1000:keys.append(3)
+				elif value < 1000:keys.append(3)
 
-			elif el < 10000:keys.append(4)
+				elif value < 10000:keys.append(4)
 
-			else:keys.append(5)
+				else:keys.append(5)
 
-		n = 1
-		for key in keys:
-			values.append(chr(int(target[n:n+key])))
-			n+=(key+1)
+			n = 1
+			for key in keys:
+				values.append(chr(int(input[n:n+key])))
+				n+=(key+1)
 
-		self.uncript_password = "".join(values)
+			self.uncode_data = "".join(values)
+			
+			return self.uncode_data
 		
-		return self.uncript_password
-		
-		
+		else:
+			print("La valeur a entre dois etre de type String")
+	
+	
+	def crypt_file(self, file_name):
+		"""
+			Code file extention 
+		"""
+		try:
+			with open(file_name, 'r') as file:
+				read = file.read()
+				self.cript(read)
+    
+			_name = file_name.split(".")[0]
+   
+			# Rename des file
+			os.rename(file_name, "{0}.inc".format(_name))
+			new_name = "{0}.inc".format(_name)
+			
+
+			# File is block
+			os.chmod("./{0}".format(new_name), stat.S_ENFMT)
+
+		except Exception as Error:
+			print(Error)
+
 
 if __name__ == "__main__":
-	enc = EncriptAll()
-
+	enc = EncryptAll()
+	print(enc.crypt("inauconf"))
+	
+	
 	
 		
